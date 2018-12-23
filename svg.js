b39cff456a3e9a7efc11d1787b08dba930dd0b78
Glitch (block-breaker-game) <none>
2018-12-23 07:16:04 +0000

    🙇🌋 Checkpoint ./svg.js:147811/462 ./renderBlocks.js:147811/729 ./index.html:147811/10

--- svg.js
+++ svg.js
@@ -1,3 +1,14 @@
+const NS = 'http://www.w3.org/2000/svg'
+
 export default function svg(tag, attrs = {}, children = []) {
+  let el = document.createElementNS(NS, tag);
   
+  for(let attr in attrs) {
+    el.setAttributeNS(NS, attr, attrs[attr])
+  }
+  
+  children.forEach( (child) => {
+    el.appendChild(child)
+  })
+  return el
 }
